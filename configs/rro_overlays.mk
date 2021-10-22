1eccec3bd9ab1de71c1c09aaf8feac4c17004d9d
Dyneteve <dyneteve@hentaios.com>
2021-10-22 20:14:07 +0200

    rro_overlays: Update from SP1A.210812.016 [squashed]
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/rro_overlays.mk
+++ configs/rro_overlays.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 The PixelDust Project
+# Copyright (C) 2020-2021 The PixelDust Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,6 +19,22 @@ PRODUCT_PACKAGES += \
     FilesOverlay \
     PixelDocumentsUIGoogleOverlay \
     PixelDustConfigOverlay \
-    PixelTetheringOverlay \
+    PixelSetupWizardOverlayHentai \
     SettingsGoogleOverlayPixelDust
+
+# Google Overlays
+PRODUCT_PACKAGES += \
+    CaptivePortalLoginOverlay \
+    CellBroadcastReceiverOverlay \
+    CellBroadcastServiceOverlay \
+    GoogleConfigOverlay \
+    GooglePermissionControllerOverlay \
+    GoogleWebViewOverlay \
+    ManagedProvisioningPixelOverlay \
+    NetworkStackOverlay \
+    PixelConfigOverlay2018 \
+    PixelConfigOverlay2019 \
+    PixelConfigOverlay2019Midyear \
+    PixelConfigOverlayCommon \
+    PixelTetheringOverlay
 
