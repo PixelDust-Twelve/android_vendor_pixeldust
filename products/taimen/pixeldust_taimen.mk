6ddfd922aecb5f86885ef820362a722e67942b73
nitin1438 <nitin.chobhe@gmail.com>
2022-04-23 05:43:42 +0000

    taimen: update make file for Android 12
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/taimen/pixeldust_taimen.mk
+++ products/taimen/pixeldust_taimen.mk
@@ -1,63 +1,85 @@
+#
+# Copyright (C) 2019-2021 The PixelDust Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# All components inherited here go to system image
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+#
+# All components inherited here go to product image
+#
+$(call inherit-product, vendor/pixeldust/build/product/pixeldust_product.mk)
+
+#
+# All components inherited here go to vendor image
+#
+# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
+# Release name
+export TARGET_DEVICE=taimen
+
 # Boot animation
 BOOTANIMATION := 1440
 
-# Release name
-PRODUCT_RELEASE_NAME := Pixel 2 XL
-export TARGET_DEVICE=taimen
+# Google Apps
+WITH_GMS := true
+DEVICE_REQUIRES_CARRIER_APPS := true
+REMOVE_GAPPS_PACKAGES += \
+    CalculatorGooglePrebuilt \
+    Photos \
+    Maps \
+    pixel_experience_2019_midyear \
+    pixel_experience_2019 \
+    pixel_experience_2020_midyear \
+    pixel_experience_2020 \
+    pixel_experience_2021_midyear \
+    pixel_experience_2021
 
-# Don't include Google apex use the AOSP variant instead 
-TARGET_EXCLUDE_GOOGLE_APEX := true
-
-# Include common PixelDust stuff
-include vendor/pixeldust/configs/pixeldust_phone.mk
-
-# Include optional stuff (e.g. prebuilt apps)
-include vendor/pixeldust/configs/system_optional.mk
-
-# Inherit device configuration
-$(call inherit-product, device/google/taimen/aosp_taimen.mk)
-
--include device/google/taimen/device-lineage.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := pixeldust_taimen
+PRODUCT_DEVICE := taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
-PRODUCT_RESTRICT_VENDOR_FILES := false
+PRODUCT_MANUFACTURER := Google
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.pixeldust.maintainer="nitin1438"
+
+# Inherit PixelDust telephony package
+$(call inherit-product, vendor/pixeldust/configs/telephony.mk)
+
+# Inherit product specific makefiles
+$(call inherit-product, device/google/taimen/device.mk)
+$(call inherit-product, vendor/google/taimen/taimen-vendor.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
     PRIVATE_BUILD_DESC="taimen-user 11 RP1A.201005.004.A1 6934943 release-keys"
 
 BUILD_FINGERPRINT := google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
-
-$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
-
-# Google Apps
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
-REMOVE_GAPPS_PACKAGES += \
-    DevicePersonalizationPrebuiltPixel2020 \
-    DevicePolicyPrebuilt \
-    DiagnosticsToolPrebuilt \
-    Drive \
-    FilesPrebuilt \
-    GCS \
-    GoogleTTS \
-    Maps \
-    NetworkPermissionConfigGoogle \
-    NetworkStackGoogle \
-    NgaResources \
-    pixel_2016_exclusive \
-    pixel_2018_exclusive.xml \
-    pixel_experience_2018 \
-    pixel_experience_2019_midyear \
-    pixel_experience_2019 \
-    pixel_experience_2020_midyear \
-    pixel_experience_2020 \
-    PrebuiltGmail \
-    RecorderPrebuilt \
-    SCONE \
-    YouTube \
-    YouTubeMusicPrebuilt \
-    WellbeingPrebuilt \
-    talkback
